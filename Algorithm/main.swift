8362351768cba9b31b7d9d90397803a33b17ada9
Alessio Brusco <alessiobrusco609@gmail.com>
2021-08-01 13:26:28 +0200

    Initial commit.

---
+++ Algorithm/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by Alessio Garzia Marotta Brusco on 01/08/2021.
+//
+
+import Foundation
+
+struct Equation {
+    var x: Double
+    var y: Double
+    var z: Double?
+    var n: Double
+
+    init(x: Double, y: Double, z: Double? = nil, n: Double) {
+        self.x = x
+        self.y = y
+        self.z = z
+        self.n = n
+    }
+}
